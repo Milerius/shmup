145318d79bd2caa5b66a067ce5c9d18f46e26a1c
Nicolas Phister <nicolas@phister.fr>
2018-02-07 22:55:35 +0100

    removed commented code in CameraSystem`

--- plugins/camera/CameraSystem.cpp
+++ plugins/camera/CameraSystem.cpp
@@ -1,4 +1,3 @@
-#include <common/components/PhysicsComponent.hpp>
 #include "CameraSystem.hpp"
 #include "EntityManager.hpp"
 #include "systems/LuaSystem.hpp"
@@ -17,16 +16,7 @@ CameraSystem::CameraSystem(kengine::EntityManager & em)
               frustrum.size = { 20, 0, 11.25 };
 
               go.attachComponent<kengine::TransformComponent3d>();
-          }))// ,
-          // _minimap(_em.createEntity<kengine::GameObject>("minimap", [](kengine::GameObject & go) {
-          //     auto & frustrum = go.attachComponent<kengine::CameraComponent3d>().frustrum;
-          //     frustrum.topLeft = { -50, 0, -50 };
-          //     frustrum.size = { 100, 0, 100 };
-
-          //     auto & box = go.attachComponent<kengine::TransformComponent3d>().boundingBox;
-          //     box.topLeft = { 0.6, 1, 0 };
-          //     box.size = { 0.4, 0, 0.4 };
-          // }))
+          }))
 {}
 
 static const auto factor = 2;
@@ -36,9 +26,6 @@ void CameraSystem::execute() noexcept {
         return;
 
     const auto & playerPos = _em.getEntity("player").getComponent<kengine::TransformComponent3d>().boundingBox.topLeft;
-    // auto & miniFrustrum = _minimap.getComponent<kengine::CameraComponent3d>().frustrum;
-    // miniFrustrum.topLeft = { playerPos.x - 2 * screenSize.x, 0, playerPos.z - 2 * screenSize.z };
-    // miniFrustrum.size = { 4 * screenSize.x, 0, 4 * screenSize.z };
 
     auto & frustrum = _camera.getComponent<kengine::CameraComponent3d>().frustrum;
     auto & pos = frustrum.topLeft;
